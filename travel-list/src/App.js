17f1a441d3ee5d2771559b523667306ae823c2f2
Clark <60542411+paranoidclark@users.noreply.github.com>
2024-09-17 11:53:27 +0800

    Sept 17 24

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+const initialItems = [
+	{id: 1, description: "Passports", quantity: 2, packed: false},
+	{id: 2, description: "Socks", quantity: 12, packed: true},
+	{id: 2, description: "Charge", quantity: 1, packed: false},
+];
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	return (
+		<div className="app">
+			<Logo />
+			<Form />
+			<PackingList />
+			<Stats />
+		</div>
+	);
 }
 
-export default App;
+function Logo() {
+	return <h1>🌏 Far Away 🎒</h1>;
+}
+
+function Form() {
+	function handleSubmit(e) {
+		e.preventDefault();
+	}
+
+	return (
+		<form className="add-form" onSubmit={handleSubmit}>
+			<h3>What do you need for your trip?</h3>
+			<select name="" id="">
+				{Array.from({length: 20}, (_, i) => i + 1).map((num) => (
+					<option value={num} key={num}>
+						{num}
+					</option>
+				))}
+			</select>
+			<input type="text" placeholder="Item..." />
+			<button>Add</button>
+		</form>
+	);
+}
+
+function PackingList() {
+	return (
+		<div className="list">
+			<ul>
+				{initialItems.map((item) => (
+					<Item itemObj={item} key={item.id} />
+				))}
+			</ul>
+		</div>
+	);
+}
+
+function Item({itemObj}) {
+	return (
+		<li>
+			<span style={itemObj.packed ? {textDecoration: "line-through"} : {}}>
+				{itemObj.quantity} {itemObj.description}
+			</span>
+			<button>❌</button>
+		</li>
+	);
+}
+
+function Stats() {
+	return (
+		<footer className="stats">
+			<em>You have X items on your list, and you already packed X </em>
+		</footer>
+	);
+}
