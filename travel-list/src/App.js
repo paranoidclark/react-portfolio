79a286fb78660c3d18d185fced840b253568d428
Clark ASUS <clarkkemparan@gmail.com>
2024-09-18 08:31:17 +0800

    Sept 18 24

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,3 +1,5 @@
+import {useState} from "react";
+
 const initialItems = [
 	{id: 1, description: "Passports", quantity: 2, packed: false},
 	{id: 2, description: "Socks", quantity: 12, packed: true},
@@ -20,21 +22,37 @@ function Logo() {
 }
 
 function Form() {
+	const [description, setDescription] = useState("");
+	const [amount, setAmount] = useState(1);
+
 	function handleSubmit(e) {
 		e.preventDefault();
+
+		if (!description) return;
+
+		const newItem = {description, amount, packed: false, id: Date.now()};
+		console.log(newItem);
+
+		setDescription("");
+		setAmount(1);
 	}
 
 	return (
 		<form className="add-form" onSubmit={handleSubmit}>
 			<h3>What do you need for your trip?</h3>
-			<select name="" id="">
+			<select value={amount} onChange={(e) => setAmount(e.target.value)}>
 				{Array.from({length: 20}, (_, i) => i + 1).map((num) => (
 					<option value={num} key={num}>
 						{num}
 					</option>
 				))}
 			</select>
-			<input type="text" placeholder="Item..." />
+			<input
+				type="text"
+				placeholder="Item..."
+				value={description}
+				onChange={(e) => setDescription(e.target.value)}
+			/>
 			<button>Add</button>
 		</form>
 	);
